0b4dc02d1c977efbd8dcec2192788ee4c0cd41ee
Robert Wood Jr <rdrwood@gmail.com>
2019-02-27 20:54:01 -0800

    Setup project

---
+++ CStringFunctions/CStringFunctions.cpp
@@ -0,0 +1,62 @@
+// Robert Wood Jr
+// Programming Assignment 8
+// CStringFunctions
+// 
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+void reverse(char *);
+int wordCount(char *);
+void nameFormatter(char *, char *, char *, char *);
+
+const int max_full_name_length = 45;
+
+int main() {
+	// test reverse function
+	char word1[] = "Gravity";
+	char word2[] = { 'w', 'o', 'r', 'l', 'd', '\0' };
+
+	cout << word1 << " reversed is ";
+	reverse(word1);
+	cout << word1 << endl;
+	cout << word2 << " reversed is ";
+	reverse(word2);
+	cout << word2 << endl;
+
+	// test wordCount function
+	char word3[] = "Four score and seven years ago";
+	cout << word3 << " contains " << wordCount(word3) << " words." << endl;
+	char word4[] = "Four";
+	cout << word4 << " contains " << wordCount(word4) << " words." << endl;
+	char word5[] = "";
+	cout << word5 << " contains " << wordCount(word5) << " words." << endl;
+
+	// test nameFormatter
+	char first[] = "Forrest";
+	char middle[] = "George";
+	char last[] = "Gump";
+	char fullName[max_full_name_length] = { '\0' };
+	cout << "Formatting " << first << " " << middle
+		<< " " << last << ": ";
+	nameFormatter(first, middle, last, fullName);
+	cout << fullName << endl;
+
+
+	system("pause");
+	return 0;
+}
+
+void reverse(char * word) {
+	// TO DO: implement this function
+}
+int wordCount(char * sentence) {
+	// TO DO: implement this function
+	return 0;
+}
+void nameFormatter(char * first, char * middle, char * last, char * fullName) {
+	// TO DO: implement this function
+}
